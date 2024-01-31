5740599e8fe57467d5e8a4d36672f8fe7172442c
AndresSamboni <andres.pag.19@gmail.com>
2024-01-30 23:50:36 -0500

    feat: New organization of the files and creation of the JavaScript file to develop the game.

---
+++ documentation/LEARNING_CSS.md
@@ -0,0 +1,26 @@
+# <FONT COLOR = "blue">***LEARNING CSS***</FONT>
+
+> [!NOTE]
+> To apply style to the tag I need to refer to this tag with its name and then in braces I can define the style I need the tag to take.
+> ```
+> body{
+>     margin: 0;
+>     padding: 0;
+>     display: grid;
+>     place-content: center;
+>     height: 90vh;;
+> }
+> ```
+
+> [!CAUTION]
+> To refer a tag I can not use **.body** for example because the style is not applied.
+
+> [!NOTE]
+> To center the content in a web page I use this inside the style of the body tag:
+> ```
+> place-content: center;
+> ```
+> To separate the content the top of web page I use this inside the style of the body tag:
+> ```
+> height: 90vh;;
+> ```
---
+++ js/tetris.js
@@ -0,0 +1,54 @@
+/*
+    To start with this project it is important to reference the HTML element canvas because this is the drawing space for the game.
+*/
+
+// Reference the canvas element.
+let canvas = document.getElementById("myCanvas");
+let context = canvas.getContext("2d");
+
+// Define the size of the board game.
+const BLOCK_SIZE = 20;
+const WITH = 10;
+const HEIGHT = 25;
+
+// Define the board version 1
+let board = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+]
+
+// Stablish the canvas dimensions.
+canvas.with = WITH * BLOCK_SIZE;
+canvas.height = HEIGHT * BLOCK_SIZE;
+context.scale( BLOCK_SIZE, BLOCK_SIZE );
+
+// Draw
+function draw () {
+    context.fillStyle = "#000";
+    context.fillRect ( 0, 0, canvas.with, canvas.height );
+}
+
+draw ();
