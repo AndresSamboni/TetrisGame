bb5703cc504c9de1975d46c8d39ca77d91636d03
AndresSamboni <andres.pag.19@gmail.com>
2024-01-31 20:47:53 -0500

    feat: Update tetris.js, in this versión I create the game loop and create the first version of the board and the draw function.

--- js/tetris.js
+++ js/tetris.js
@@ -1,4 +1,37 @@
 /*
+    To create a game is important the game loop, then I use the function requestAnimationFrame() with the update function.
+
+    In this space I defined all the functions that I need and I use.
+*/
+
+// Function to update a frame in the game.
+function update () {
+    // Call the function draw.
+    draw ();
+    // Game loop.
+    requestAnimationFrame(update);
+}
+
+// Function to draw in the canvas
+function draw () {
+    // Draw the back-ground board.
+    context.fillStyle = "black";
+    context.fillRect ( 0, 0, canvas.width, canvas.height );
+    
+    // Draw the board game.
+    board.forEach( (column, j) => {
+        column.forEach( (row, i) => {
+            // Validates whether the pixel to be drawn is an occupied pixel (1) or not (0).
+            if ( row == 1) {
+                context.fillStyle = "yellow";
+                context.fillRect ( i, j, 1, 1 );
+            }
+        });
+    });
+    
+}
+
+/*
     To start with this project it is important to reference the HTML element canvas because this is the drawing space for the game.
 */
 
@@ -8,47 +41,42 @@ let context = canvas.getContext("2d");
 
 // Define the size of the board game.
 const BLOCK_SIZE = 20;
-const WITH = 10;
+const WIDTH = 15;
 const HEIGHT = 25;
 
-// Define the board version 1
-let board = [
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-]
-
 // Stablish the canvas dimensions.
-canvas.with = WITH * BLOCK_SIZE;
+canvas.width = WIDTH * BLOCK_SIZE;
 canvas.height = HEIGHT * BLOCK_SIZE;
 context.scale( BLOCK_SIZE, BLOCK_SIZE );
 
-// Draw
-function draw () {
-    context.fillStyle = "#000";
-    context.fillRect ( 0, 0, canvas.with, canvas.height );
-}
+// Define the board game
+let board = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 1]
+];
 
-draw ();
+// Call the function update() to start the game.
+update();
